f4febb5e8ea2a8a1efe404380e02dc9e2a37a208
gwetcokypj <renan.bertolazzi+iot@gmail.com>
2016-11-18 16:38:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,4 +17,6 @@ $( document ).ready(function() {
 	$('#myBtn_13').click(function() {$.getq('queue','/panTiltControl?params=13');});
 	$('#myBtn_14').click(function() {$.getq('queue','/panTiltControl?params=14');});
 	$('#myBtn_15').click(function() {$.getq('queue','/panTiltControl?params=15');});
+	$('#myBtn_16').click(function() {$.getq('queue','/panTiltControl?params=15');});
+	$('#myBtn_17').click(function() {$.getq('queue','/panTiltControl?params=15');});
 });
