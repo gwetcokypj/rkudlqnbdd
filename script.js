f387a7d69e82a94734e6a4d94537e7e919be264d
gwetcokypj <renan.bertolazzi+iot@gmail.com>
2016-11-18 14:47:49 -0500

    Update and rename .gitignore to script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+	$('#btn_on15').click(function() {$.getq('queue','/digital/15/1');});
+	$('#btn_off15').click(function() {$.getq('queue','/digital/15/0');});
+	$('#myBtn_0').click(function() {$.getq('queue','/panTiltControl?params=0');});
+	$('#myBtn_1').click(function() {$.getq('queue','/panTiltControl?params=1');});
+	$('#myBtn_2').click(function() {$.getq('queue','/panTiltControl?params=2');});
+	$('#myBtn_3').click(function() {$.getq('queue','/panTiltControl?params=3');});
+	$('#myBtn_4').click(function() {$.getq('queue','/panTiltControl?params=4');});
+	$('#myBtn_5').click(function() {$.getq('queue','/panTiltControl?params=5');});
+	$('#myBtn_6').click(function() {$.getq('queue','/panTiltControl?params=6');});
+	$('#myBtn_7').click(function() {$.getq('queue','/panTiltControl?params=7');});
+	$('#myBtn_8').click(function() {$.getq('queue','/panTiltControl?params=8');});
+	$('#myBtn_9').click(function() {$.getq('queue','/panTiltControl?params=9');});
+	$('#myBtn_10').click(function() {$.getq('queue','/panTiltControl?params=10');});
+	$('#myBtn_11').click(function() {$.getq('queue','/panTiltControl?params=11');});
+	$('#myBtn_12').click(function() {$.getq('queue','/panTiltControl?params=12');});
+	$('#myBtn_13').click(function() {$.getq('queue','/panTiltControl?params=13');});
+	$('#myBtn_14').click(function() {$.getq('queue','/panTiltControl?params=14');});
+	$('#myBtn_15').click(function() {$.getq('queue','/panTiltControl?params=15');});
+});
