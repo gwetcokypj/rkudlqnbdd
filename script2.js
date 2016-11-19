6285dac62575451eb902b7b4314193ca9e9cfbd5
gwetcokypj <renan.bertolazzi+iot@gmail.com>
2016-11-19 00:20:10 -0500

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+	$('#myBtn_0').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=0');});
+	$('#myBtn_1').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=1');});
+	$('#myBtn_2').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=2');});
+	$('#myBtn_3').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=3');});
+	$('#myBtn_4').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=4');});
+	$('#myBtn_5').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=5');});
+	$('#myBtn_6').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=6');});
+	$('#myBtn_7').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=7');});
+	$('#myBtn_8').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=8');});
+	$('#myBtn_9').click(function() {$.getq('http://rkudlqnbdd.ddns.net/panTiltControl?params=9');});
+});
